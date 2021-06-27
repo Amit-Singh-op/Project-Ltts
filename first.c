b793c1e38585e51dc2698d253cfb07b70474125f
Amit Kumar Singh <amitkmwsingh2857421@gmail.com>
2021-06-27 15:30:29 +0530

    second

--- first.c
+++ first.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    printf("hello");
+    char *arr = (char *)malloc(1000 * sizeof(char));
+    int i = 0;
+    char c;
+    while (c != '\n')
+    {
+        c = getchar();
+        arr[i++] = c;
+    }
+    a[i] = '\0';
+    printf("%s", arr);
 }
