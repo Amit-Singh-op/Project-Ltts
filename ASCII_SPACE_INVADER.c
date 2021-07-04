65c6b1a41208c932b637d0095cda7ecb3b79877b
Amit Kumar Singh <amitkmwsingh2857421@gmail.com>
2021-07-04 16:21:20 +0530

    Update

--- ASCII_SPACE_INVADER.c
+++ ASCII_SPACE_INVADER.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
-#include <string.h>
-#include <stdbool.h>
+#include <conio.h>
 #include <windows.h>
+#include <stdbool.h>
+#include <time.h>
 void delay(unsigned int milliseconds)
 {
     clock_t start = clock();
     while ((clock() - start) * 1000 / CLOCKS_PER_SEC < milliseconds)
         ;
 }
-
 int main()
 {
-    //started
-    int size_of_y = 25;
-    int size_of_x = 40;
+    int sizey = 23;
+    int sizex = 40;
     int x, y, yi;
-    char earth[size_of_y][size_of_x];
+    char world[sizey][sizex];
     char player = 'A';
-    char player_gun = '^';
-    char enemy = '*';
-    char enemy_with_shield = 'x';
-    char enemy_with_laser_power = '.';
-    char explosion = '-';
+    char playerLaser = '^';
+    char enemy = 'M';
+    char enemyShielded = 'O';
+    char enemyLaser = 'U';
+    char explosion = 'X';
+    int score = 0;
+    int victory = 1;
+    int laserReady = 1;
+    int enemyReady = 0;
 
     srand(time(NULL));
 
-    printf("\n\n\n");
+    printf("\n\n\n\n\n\n");
     /*
     printf("\t\t\t\t                _ _   _                     _               \n");
     printf("\t\t\t\t  __ _ ___  ___(_|_) (_)_ ____   ____ _  __| | ___ _ __ ___ \n");
@@ -60,14 +62,14 @@ int main()
     printf("\n \n \n \n Press any key to start.");
 
     */
-    printf("\n\n");
+    printf("\n\n\n");
     int c = 0;
-    
-    char s[] = {'\n','\n','\n','\n','\t','\t','"','C', 'O', 'M', 'M', 'A', 'N', 'D','E','R','"',':', '\n',
-                '\t','\t','\t','\t','W', 'e', 'l', 'c', 'o', 'm', 'e', ' ', 'S', 'o', 'l', 'd', 'i', 'e', 'r', '!','!','!', ' ',
-                '.', '.', '.', '.', '.', '.', '.',' ',
-                'W', 'e', ' ', 'g', 'o', 't', 't', 'a', ' ', 's', 'a', 'v', 'e', ' ', 't', 'h', 'e', ' ', 'w', 'o', 'r', 'l', 'd','!','!','!',' ',
-                'S', 'o', ' ', 'L', 'e', 't','s', ' ', 'd', 'o', ' ', 'i', 't','!','!','!',' ',
+
+    char s[] = {'\n', '\n', '\n', '\n', '\n', '\t', '\t', '"', 'C', 'O', 'M', 'M', 'A', 'N', 'D', 'E', 'R', '"', ':',
+                '\t', 'W', 'e', 'l', 'c', 'o', 'm', 'e', ' ', 'S', 'o', 'l', 'd', 'i', 'e', 'r', '!', '!', '!', ' ',
+                '.', '.', '.', '.', '.', '.', '.', ' ',
+                'W', 'e', ' ', 'g', 'o', 't', 't', 'a', ' ', 's', 'a', 'v', 'e', ' ', 't', 'h', 'e', ' ', 'w', 'o', 'r', 'l', 'd', '!', '!', '!', ' ',
+                'S', 'o', ' ', 'L', 'e', 't', 's', ' ', 'd', 'o', ' ', 't', 'h', 'i', 's', '!', '!', '!', ' ',
                 '\0'};
     for (c = 0; s[c] != '\0'; c++)
     {
@@ -77,12 +79,53 @@ int main()
     Sleep(1000);
     printf("\n\n\n\n\n\n\t\t Press any key to start.");
     getch();
-    
-    
-   // system("MODE 45,25");
-   //	ShowScrollBar(GetConsoleWindow(), SB_VERT, 0);
-	
-	//printf("helllllo");
 
-    /*initialise earth*/
+    /*initialise world*/
+    int totalEnemies = 0;
+    for (x = 0; x < sizex; x++)
+    {
+        for (y = 0; y < sizey; y++)
+        {
+            if ((y + 1) % 2 == 0 && y < 7 && x > 4 && x < sizex - 5 && x % 2 == 0)
+            {
+                world[y][x] = enemy;
+                totalEnemies++;
+            }
+            else if ((y + 1) % 2 == 0 && y >= 7 && y < 9 && x > 4 && x < sizex - 5 && x % 2 == 0)
+            {
+                world[y][x] = enemyShielded;
+                totalEnemies = totalEnemies + 2;
+            }
+            else
+            {
+                world[y][x] = ' ';
+            }
+        }
+    }
+    world[sizey - 1][sizex / 2] = player;
+    int i = 1;
+    char direction = 'l';
+    char keyPress;
+    int currentEnemies = totalEnemies;
+    while (currentEnemies > 0 && victory)
+    {
+        int drop = 0;
+        int enemySpeed = 1 + 10 * currentEnemies / totalEnemies;
+        laserReady++;
+
+        /*display world*/
+        system("cls");
+        printf("     SCORE:    %d", score);
+        printf("\n");
+        for (y = 0; y < sizey; y++)
+        {
+            printf("|");
+            for (x = 0; x < sizex; x++)
+            {
+                printf("%c", world[y][x]);
+            }
+            printf("|");
+            printf("\n");
+        }
+    }
 }
