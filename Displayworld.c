cc99b43a3007b72b5df07fbebf65c5137bbb88d0
Amit Kumar Singh <amitkmwsingh2857421@gmail.com>
2021-07-06 12:37:23 +0530

    Added make file

--- Displayworld.c
+++ Displayworld.c
@@ -5,19 +5,32 @@
 void display(int sizex, int sizey, int score, char **world)
 {
     system("cls");
-    textcolor(4);
+    textcolor(11);
     printf("     SCORE:    %d", score);
     printf("\n\n");
     for (int y = 0; y < sizey; y++)
     {
-        textcolor(6);
+        textcolor(8);
         printf("\t\t\t\t\t|");
         for (int x = 0; x < sizex; x++)
         {
-            textcolor(5);
-            printf("%c", world[y][x]);
+            if (world[y][x] == 'E')
+            {
+                textcolor(12);
+                printf("%c", world[y][x]);
+            }
+            else if (world[y][x] == 'B')
+            {
+                textcolor(7);
+                printf("%c", world[y][x]);
+            }
+            else
+            {
+                textcolor(14);
+                printf("%c", world[y][x]);
+            }
         }
-        textcolor(6);
+        textcolor(8);
         printf("|");
         printf("\n");
     }
--- Makefile
+++ Makefile
@@ -1,7 +1,5 @@
-all: hello.exe
+target : ASCII_SPACE_INVADER.c Initialise_world.c Displayworld.c Color.c Loading.c fun.h
+	gcc ASCII_SPACE_INVADER.c Initialise_world.c Displayworld.c Color.c Loading.c -o ASCII_SPACE_INVADER.exe
 
-hello.exe: ASCII_SPACE_INVADER.c Initialise_world.c Displayworld.c Color.c Loading.c
-	 gcc -c ASCII_SPACE_INVADER.c Initialise_world.c Displayworld.c Color.c Loading.c
-     
-clean:
-	 rm hello.exe
+run: target
+	./ASCII_SPACE_INVADER.exe
