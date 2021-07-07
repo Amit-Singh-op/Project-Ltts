9e7f7220a5455a031aa9cece797851b4c8b1ab3f
Amit Kumar Singh <amitkmwsingh2857421@gmail.com>
2021-07-07 18:59:47 +0530

    unity folder added

--- 3_Implementation/unity/unity_internals.h
+++ 3_Implementation/unity/unity_internals.h
@@ -1,5 +1,5 @@
 /* ==========================================
-    Unity Project - A Test Framework for C
+    Unity Project - A Test Framework for C/c++
     Copyright (c) 2007-21 Mike Karlesky, Mark VanderVoord, Greg Williams
     [Released under MIT License. Please refer to license.txt for details]
 ========================================== */
