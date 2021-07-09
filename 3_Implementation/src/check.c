08423e892e0d110bae119b0685762d914abedf36
Amit Kumar Singh <amitkmwsingh2857421@gmail.com>
2021-07-09 21:56:28 +0530

    updated

---
+++ 3_Implementation/src/check.c
@@ -0,0 +1,32 @@
+#include "fun.h";
+int i_value(int wrong)
+{
+    if (wrong == 1)
+    {
+        return (2);
+    }
+    if (wrong == 2)
+    {
+        return (3);
+    }
+    if (wrong == 3)
+    {
+        return (4);
+    }
+    if (wrong == 4)
+    {
+        return (3);
+    }
+    if (wrong == 5)
+    {
+        return (3);
+    }
+    if (wrong == 6)
+    {
+        return (5);
+    }
+    if (wrong == 7)
+    {
+        return (5);
+    }
+}
--- 3_Implementation/test/test.c
+++ 3_Implementation/test/test.c
@@ -7,13 +7,20 @@ void setUp() {}
 /* Required by the unity test framework */
 void tearDown() {}
 
-void check(void)
+void i_run()
 {
+    TEST_ASSERT_EQUAL(2, i_value(1));
+    TEST_ASSERT_EQUAL(3, i_value(2));
+    TEST_ASSERT_EQUAL(4, i_value(3));
+    TEST_ASSERT_EQUAL(3, i_value(4));
+    TEST_ASSERT_EQUAL(3, i_value(5));
+    TEST_ASSERT_EQUAL(5, i_value(6));
+    TEST_ASSERT_EQUAL(5, i_value(7));
 }
 
 int main()
 {
     UNITY_BEGIN();
-    RUN_TEST(check);
+    RUN_TEST(i_run);
     return UNITY_END();
 }
